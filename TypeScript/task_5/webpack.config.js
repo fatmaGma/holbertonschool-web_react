33abd839a108fd45b5523e405188852e596c566e
fatmaGma <8891@holbertonstudents.com>
2025-07-16 12:14:04 +0200

    complet

---
+++ TypeScript/task_5/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './js/main.ts',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,  // Ensure this is available in Webpack 5
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	devServer: {
+		static: './dist',  // Serve files from the 'dist' directory
+		hot: true,         // Enable Hot Module Replacement (HMR)
+		open: true,        // Automatically open the browser
+		port: 8080,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',  // Ensure this file exists
+		}),
+	],
+	mode: 'development',
+};
+
