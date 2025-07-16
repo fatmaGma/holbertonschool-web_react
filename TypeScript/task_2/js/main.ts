f0357600743cfad666aae9b1d84149d257dec157
fatmaGma <8891@holbertonstudents.com>
2025-07-16 11:56:42 +0200

    adjustment done

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,17 +1,100 @@
-/// <reference path="./crud.d.ts" />
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
 
-import { RowID, RowElement } from './interface';
-import * as CRUD from './crud';
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
 
-// Création d'un objet row avec les valeurs spécifiées
-const row: RowElement = { firstName: 'Guillaume', lastName: 'Salva' };
+class Director implements DirectorInterface {
+	workFromHome(): string {
+		return "Working from home";
+	}
+	getCoffeeBreak(): string {
+		return "Getting a coffee break";
+	}
+	workDirectorTasks(): string {
+		return "Getting to director tasks";
+	}
+}
 
-// Insertion de la ligne et assignation de la valeur à 'newRowID'
-const newRowID: RowID = CRUD.insertRow(row);
+class Teacher implements TeacherInterface {
+	workFromHome(): string {
+		return "Cannot work from home";
+	}
+	getCoffeeBreak(): string {
+		return "Cannot have a break";
+	}
+	workTeacherTasks(): string {
+		return "Getting to work";
+	}
+}
 
-// Mise à jour de la ligne avec un âge
-const updatedRow: RowElement = { firstName: 'Guillaume', lastName: 'Salva', age: 23 };
-CRUD.updateRow(newRowID, updatedRow);
+function createEmployee(salary: number | string): Director | Teacher {
+	if (typeof salary === "number" && salary < 500) {
+		return new Teacher();
+	} else {
+		return new Director();
+	}
+}
 
-// Suppression de la ligne
-CRUD.deleteRow(newRowID);
+
+function isDirector(employee: Director | Teacher): employee is Director {
+	return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): void {
+	if (isDirector(employee)) {
+		console.log(employee.workDirectorTasks());
+		displayResult(employee.workDirectorTasks());
+	} else {
+		console.log(employee.workTeacherTasks());
+		displayResult(employee.workTeacherTasks());
+	}
+}
+
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+	if (todayClass === "Math") {
+		return "Teaching Math";
+	} else {
+		return "Teaching History";
+	}
+}
+
+
+function displayResult(result: string): void {
+	const body = document.querySelector('body');
+	const p = document.createElement('p');
+	p.textContent = result;
+	if (body) {
+		body.appendChild(p);
+	}
+}
+
+
+const employee1 = createEmployee(200);
+const employee2 = createEmployee(1000);
+const employee3 = createEmployee('$500');
+
+console.log(employee1);
+console.log(employee2);
+console.log(employee3);
+
+executeWork(employee1);
+executeWork(employee2);
+executeWork(employee3);
+
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
+
+displayResult(teachClass('Math'));
+displayResult(teachClass('History'));
+
